a6b254a9a2c8c2eb72ca6fc41937553839440d4e
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2015-04-08 14:48:27 -0700

    ZoomInOut-Crop:  #still working version (NOW: they're in the middle, but cannot crop it for now)

--- FormalChat/app/src/main/java/com/android/formalchat/CropActivity.java
+++ FormalChat/app/src/main/java/com/android/formalchat/CropActivity.java
@@ -1,13 +1,11 @@
 package com.android.formalchat;
 
 import android.app.Activity;
-import android.content.ClipData;
-import android.content.ClipDescription;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
-import android.media.Image;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.DragEvent;
@@ -15,84 +13,141 @@ import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 /**
  * Created by Sve on 3/31/15.
  */
-public class CropActivity extends Activity implements View.OnDragListener, View.OnTouchListener{
-    private View dragedRectangleView;
+public class CropActivity extends Activity {
+    private ImageView windowRectangleView;
     private ImageView original;
     private android.widget.RelativeLayout.LayoutParams layoutParams;
+    private ZoomInOutImgView imgView;
+    private Button doneEditing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crop_layout);
 
-        dragedRectangleView =  findViewById(R.id.dragRect);
-        original = (ImageView) findViewById(R.id.image);
+//        dragedRectangleView =  findViewById(R.id.dragRect);
+        //original = (ImageView) findViewById(R.id.image);
 
-        dragedRectangleView.setTag("ImgToCrop");
-        dragedRectangleView.setOnTouchListener(this);
-        //findViewById(R.id.root).setOnDragListener(this);
-        findViewById(R.id.image).setOnDragListener(this);
-    }
+        final Drawable drawable = getResources().getDrawable(R.drawable.ic_launcher);
+        final FrameLayout rl = (FrameLayout) findViewById(R.id.root_crop);
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+        final LinearLayout.LayoutParams params_original = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
-            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
-            v.startDrag(null, shadowBuilder, v, 0);
-            v.setVisibility(View.INVISIBLE);
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    @Override
-    public boolean onDrag(View v, DragEvent event) {
+        ViewTreeObserver viewTreeObserver = rl.getViewTreeObserver();
+        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                removeOnGlobalLayoutListener(rl, this);
+                int posX = rl.getMeasuredWidth();
+                int posY = rl.getMeasuredHeight();
 
-        if (event.getAction()==DragEvent.ACTION_DROP) {
-            View view = (View) event.getLocalState();
-            ViewGroup from = (ViewGroup) view.getParent();
-            from.removeView(view);
+                imgView = new ZoomInOutImgView(getApplicationContext(), drawable, posX, posY);
+                imgView.setScaleType(ImageView.ScaleType.FIT_XY);
+                imgView.setLayoutParams(params_original);
+                imgView.setOnDragListener(new View.OnDragListener() {
+                    @Override
+                    public boolean onDrag(View v, DragEvent event) {
+                        return onDrag(v, event);
+                    }
+                });
+                rl.addView(imgView);
 
-            if(v == original) {
-                RelativeLayout to = (RelativeLayout) findViewById(R.id.root);
+                windowRectangleView = (ImageView) findViewById(R.id.dragRect);
+                windowRectangleView.bringToFront();
 
-                view.setX(event.getX() - (view.getWidth()/2));
-                view.setY(event.getY() - (view.getHeight() / 2));
-
-                to.addView(view);
-                view.setVisibility(View.VISIBLE);
+                doneEditing = (Button) findViewById(R.id.done_editing);
+                doneEditing.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
 
 
+                        Log.v("formalchat", "posX = " + imgView.getPosX() + "   posY = " + imgView.getPosY() +
+                        "   width = " + imgView.getWidth() + "   height = " + imgView.getHeight());
 
-
-                Bitmap bitmap1 = Bitmap.createScaledBitmap(
-                        ((BitmapDrawable) original.getDrawable())
-                                .getBitmap(), original.getWidth(), original
-                                .getHeight(), false);
-
-
-
-                Bitmap bitmap = Bitmap.createBitmap(bitmap1,
-                        (int) view.getX(), (int) view.getY(), view.getWidth(),
-                        view.getHeight());
-
-                //view.setBackground(new BitmapDrawable(getResources(), bitmap));
-                Intent intent = new Intent(this, CropImageActivity.class);
-                intent.putExtra("BitmapImage", bitmap);
-                startActivity(intent);
+                        Log.v("formalchat", "posX = " + (int) windowRectangleView.getX() + "   posY = " + (int) windowRectangleView.getY() +
+                                "   width = " + windowRectangleView.getWidth() + "   height = " + windowRectangleView.getHeight());
+                    }
+                });
             }
+        });
 
+
+
+
+//        dragedRectangleView.setOnTouchListener(this);
+        //findViewById(R.id.root).setOnDragListener(this);
+//        findViewById(R.id.image).setOnDragListener(this);
+    }
+
+
+//    public boolean onTouch(View v, MotionEvent event) {
+//        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//
+//            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+//            v.startDrag(null, shadowBuilder, v, 0);
+//            v.setVisibility(View.INVISIBLE);
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    public boolean onDrag(View v, DragEvent event) {
+//
+//        if (event.getAction()==DragEvent.ACTION_DROP) {
+//            View view = (View) event.getLocalState();
+//            ViewGroup from = (ViewGroup) view.getParent();
+//            from.removeView(view);
+//
+//            if(v == original) {
+//                FrameLayout to = (FrameLayout) findViewById(R.id.root_crop);
+//
+//                view.setX(event.getX() - (view.getWidth()/2));
+//                view.setY(event.getY() - (view.getHeight() / 2));
+//
+//                to.addView(view);
+//                view.setVisibility(View.VISIBLE);
+//
+//
+//                Bitmap bitmap1 = Bitmap.createScaledBitmap(
+//                        ((BitmapDrawable)
+//                                original.getDrawable()).getBitmap(),
+//                                original.getWidth(),
+//                        original.getHeight(), false);
+//
+//
+//
+//                Bitmap bitmap = Bitmap.createBitmap(bitmap1,
+//                        (int) view.getX(), (int) view.getY(), view.getWidth(),
+//                        view.getHeight());
+//
+//                //view.setBackground(new BitmapDrawable(getResources(), bitmap));
+//                Intent intent = new Intent(this, CropImageActivity.class);
+//                intent.putExtra("BitmapImage", bitmap);
+//                startActivity(intent);
+//            }
+//
+//        }
+//        return true;
+//    }
+
+    public static void removeOnGlobalLayoutListener(View v, ViewTreeObserver.OnGlobalLayoutListener listener){
+        if (Build.VERSION.SDK_INT < 16) {
+            v.getViewTreeObserver().removeGlobalOnLayoutListener(listener);
+        } else {
+            v.getViewTreeObserver().removeOnGlobalLayoutListener(listener);
         }
-        return true;
     }
 }
--- FormalChat/app/src/main/java/com/android/formalchat/ZoomInOutImgView.java
+++ FormalChat/app/src/main/java/com/android/formalchat/ZoomInOutImgView.java
@@ -1,20 +1,28 @@
 package com.android.formalchat;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Display;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Sve on 4/1/15.
  */
-public class ZoomInOutImgView extends View {
+public class ZoomInOutImgView extends ImageView {
     private static final int INVALID_POINTER_ID = -1;
 
     private Drawable image;
@@ -27,10 +35,19 @@ public class ZoomInOutImgView extends View {
     private ScaleGestureDetector scaleGestureDetector;
     private float scaleFactor = 1.f;
 
+    private int size;
+    private int pX;
+    private int pY;
+
 //    public ZoomInOutImgView(Context context) {
 //        this(context, null, 0);
 //        image = getResources().getDrawable(context.getResources().getIdentifier("background", "drawable", context.getPackageName()));
 //        image.setBounds(50, 50, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+//
+//        RelativeLayout rl = (RelativeLayout)findViewById(R.id.root_crop);
+//        posX = rl.getWidth()/2;
+//        posY = rl.getHeight()/2;
+//        Log.v("formalchat", "posX = "+posX+"   posY = "+posY);
 //    }
 //
 //    public ZoomInOutImgView(Context context, AttributeSet attrs) {
@@ -43,13 +60,33 @@ public class ZoomInOutImgView extends View {
 //    }
 
 
-    public ZoomInOutImgView(Context context, Drawable image, int left, int top, int right, int bottom) {
+    public ZoomInOutImgView(Context context, Drawable image, int pX, int pY) {
         super(context);
         this.image = image;
-        image.setBounds(left, top, right, bottom);
+        int pxs = (int) (100 * getResources().getDisplayMetrics().density);
+        image.setBounds(0, 0, pxs, pxs);
         scaleGestureDetector = new ScaleGestureDetector(context, new ScaleListener());
 
+        posX = pX / 2 - pxs / 2;
+        posY = pY / 2 - pxs / 2;
     }
+
+
+//    @Override
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//        size = 0;
+//        int width = getMeasuredWidth();
+//        int height = getMeasuredHeight();
+//
+//        if (width > height) {
+//            size = height;
+//        } else {
+//            size = width;
+//        }
+//
+//        setMeasuredDimension(size, size);
+//    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -70,7 +107,7 @@ public class ZoomInOutImgView extends View {
                 float y = event.getY(pointerIndex);
 
                 //Only move is scaleGestureDetector isn't processing a gesture
-                if(!scaleGestureDetector.isInProgress()) {
+                if (!scaleGestureDetector.isInProgress()) {
                     float dx = x - lastTouchX;
                     float dy = y - lastTouchY;
                     posX += dx;
@@ -92,7 +129,7 @@ public class ZoomInOutImgView extends View {
             case MotionEvent.ACTION_POINTER_UP: {
                 int pointerIndex = (event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
                 int pointerId = event.getPointerId(pointerIndex);
-                if(pointerId == activePointerId) {
+                if (pointerId == activePointerId) {
                     // This was our active pointer going up. Choose a new
                     // active pointer and adjust accordingly.
 
@@ -116,8 +153,22 @@ public class ZoomInOutImgView extends View {
         canvas.translate(posX, posY);
         canvas.scale(scaleFactor, scaleFactor);
         image.draw(canvas);
+
         canvas.restore();
     }
+
+    public Drawable getImageDrawable() {
+        return image;
+    }
+
+    public int getPosX() {
+        return (int) posX;
+    }
+
+    public int getPosY() {
+        return (int) posY;
+    }
+
 
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
