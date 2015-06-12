9cfa1b87846a15674a5d84068f9dab45f4ae209a
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2015-06-12 15:36:42 -0700

    Profile Gallery : refresh grid gallery after delete

--- FormalChat/app/src/main/java/com/android/formalchat/ProfileGallery.java
+++ FormalChat/app/src/main/java/com/android/formalchat/ProfileGallery.java
@@ -2,10 +2,12 @@ package com.android.formalchat;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.GridView;
 
@@ -16,13 +18,18 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * Created by Sve on 5/21/15.
  */
 public class ProfileGallery extends Activity {
+    private static final String PREFS_NAME = "FormalChatPrefs";
+    private SharedPreferences sharedPreferences;
     private Activity activity;
     private GridView gridView;
     private boolean videoExists;
@@ -37,6 +44,7 @@ public class ProfileGallery extends Activity {
         getActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
 
         activity = this;
+        sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
         gridView = (GridView) findViewById(R.id.grid_layout_profile);
         imagePaths = new ArrayList<>();
         user = ParseUser.getCurrentUser();
@@ -72,6 +80,48 @@ public class ProfileGallery extends Activity {
                     }
                 }
             });
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        if(sharedPreferences.contains("refresh") && sharedPreferences.getBoolean("refresh", false)) {
+            File dir = Environment.getExternalStorageDirectory();
+            String filePath = "/.formal_chat";
+
+            File dir_folder = new File(dir, filePath);
+            File[] files = dir_folder.listFiles();
+            Arrays.sort(files, new Comparator<File>() {
+                @Override
+                public int compare(File file1, File file2) {
+                    if (file1.lastModified() > file2.lastModified()) {
+                        return 1;
+                    }
+                    else if (file1.lastModified() < file2.lastModified()) {
+                        return -1;
+                    }
+                    else {
+                        return 0;
+                    }
+                }
+            });
+
+            ArrayList<String> imagePaths = new ArrayList<>();
+            for(File f : files) {
+                if("IMG".equals(f.getName().substring(0, 3))) {
+                    imagePaths.add("file:" + f.getAbsolutePath());
+                }
+            }
+
+            galleryAdapter.updateImages(imagePaths);
+            sharedPreferences.edit().putBoolean("refresh", false).commit();
         }
     }
 
