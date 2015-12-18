8f2420d493920aba332a5fb909f9e9d8325d8472
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2015-12-17 17:32:39 -0800

    Base Activity: onResume check if 5 mins passed and if yes update current's user LastSeen field in Parse

--- FormalChat/app/src/main/java/com/android/formalchat/BaseActivity.java
+++ FormalChat/app/src/main/java/com/android/formalchat/BaseActivity.java
@@ -4,16 +4,30 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.parse.ParseUser;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Sve on 12/15/15.
  */
 public class BaseActivity extends AppCompatActivity {
+    private static final int MILLIS_IN_SECOND = 1000;
+    private static final int SECONDS_IN_MINUTE = 60;
+    private static final int MINUTES = 5; // minutes in which to update last seen
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    private ParseUser currentUser;
 
     public boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -36,5 +50,66 @@ public class BaseActivity extends AppCompatActivity {
 //        tv.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), colorId));
         tv.setTextSize(16);
         return snackbar;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        currentUser = ParseUser.getCurrentUser();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateLastSeen();
+    }
+
+    private void updateLastSeen() {
+        if(currentUser != null) {
+            String lastSeenTimeStamp = (String)currentUser.get("lastSeen");
+            Log.v(ApplicationOfficially.TAG, "Last seen = " + lastSeenTimeStamp);
+
+            if(lastSeenTimeStamp != null) {
+                long lastSeenInMillis = getLastSeenInMillis(lastSeenTimeStamp);
+                if(lastSeenInMillis != 0) {
+                    if(isTimeForUpdate(lastSeenInMillis)) {
+                        updateParseUserLastSeen();
+                    }
+                }
+            }
+            else {
+                updateParseUserLastSeen();
+            }
+        }
+    }
+
+    private void updateParseUserLastSeen() {
+        if(currentUser != null) {
+            currentUser.put("lastSeen", dateFormat.format(new Date(System.currentTimeMillis())));
+            currentUser.saveInBackground();
+        }
+    }
+
+    private boolean isTimeForUpdate(long lastSeenInMillis) {
+        long updateIn = MILLIS_IN_SECOND * SECONDS_IN_MINUTE * MINUTES;
+        Log.v(ApplicationOfficially.TAG, "Updating last seen : " + Math.abs(lastSeenInMillis - System.currentTimeMillis()) + " > " + updateIn);
+
+        if(Math.abs(lastSeenInMillis - System.currentTimeMillis()) > updateIn) {
+            return true;
+        }
+        return false;
+    }
+
+    private long getLastSeenInMillis(String lastSeenTimeStamp) {
+        Date resultDate;
+        try {
+            resultDate = dateFormat.parse(lastSeenTimeStamp);
+            return resultDate.getTime();
+        }
+        catch (ParseException ex) {
+            Log.e(ApplicationOfficially.TAG, "Get Last Seen In Millis exception : " + ex.getMessage());
+        }
+
+        return 0;
     }
 }
